c4267449de2c760ab727b8c10e6d2ca11d4023ac
Alfred Ayre <alfred.w.ayre@gmail.com>
2024-01-02 10:37:43 +0100

    Add basic questions - javascript

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // JS Script
+const questions = [
+  {
+    question: "Generic Question 1",
+    answers: [
+      { text: "Like this", bodytype: "Hourglass" },
+      { text: "Like that", bodytype: "Apple" },
+      { text: "With this", bodytype: "Pear" },
+      { text: "Without that", bodytype: "Triangle" },
+    ],
+  },
+  {
+    question: "Generic Question 2",
+    answers: [
+      { text: "Like this", bodytype: "Hourglass" },
+      { text: "Like that", bodytype: "Apple" },
+      { text: "With this", bodytype: "Pear" },
+      { text: "Without that", bodytype: "Triangle" },
+    ],
+  },
+  {
+    question: "Generic Question 3",
+    answers: [
+      { text: "Like this", bodytype: "Hourglass" },
+      { text: "Like that", bodytype: "Apple" },
+      { text: "With this", bodytype: "Pear" },
+      { text: "Without that", bodytype: "Triangle" },
+    ],
+  },
+  {
+    question: "Generic Question 4",
+    answers: [
+      { text: "Like this", bodytype: "Hourglass" },
+      { text: "Like that", bodytype: "Apple" },
+      { text: "With this", bodytype: "Pear" },
+      { text: "Without that", bodytype: "Triangle" },
+    ],
+  },
+];
